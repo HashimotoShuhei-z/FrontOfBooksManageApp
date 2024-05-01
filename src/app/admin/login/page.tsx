6dfde6562e396300d06bec9ce7f4517ae5dc6bbb
Shuhei Hashimoto <159013664+HashimotoShuhei-z@users.noreply.github.com>
2024-05-02 00:10:01 +0900

    cookieに認証トークンを保持していた場合、ホームにリダイレクトさせたい

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,10 +1,34 @@
+'use client'
 import Link from 'next/link'
-import React from 'react'
-import { Button, buttonVariants } from '@/components/ui/button'
-import AdminLoginForm from '@/components/original/adminLoginForm'
+import React, { useEffect, useState } from 'react'
+import { buttonVariants } from '@/components/ui/button'
+import { useRouter } from 'next/navigation'
 
-//TODO:ログイン処理を実装する
+import AdminLoginForm from '@/components/original/adminLoginForm'
+//もしcookieに認証トークンを保持していた場合、ホームにリダイレクトさせたい
+//すべてのクッキーをオブジェクトとして取得
+export const getAllCookies = (): Record<string, string> => {
+  const cookies = document.cookie.split('; ').reduce((acc, cookie) => {
+    const [name, value] = cookie.split('=')
+    return { ...acc, [name]: value }
+  }, {})
+  return cookies
+}
+
 const page = () => {
+  const router = useRouter()
+  //useEffectはコンポーネントが画面に表示された瞬間に処理をしてくれる
+  useEffect(() => {
+    const cookies = getAllCookies()
+    //トークン取得
+    const token = cookies.token
+    // トークンが存在する場合、ホーム画面に自動的に遷移させる
+    if (token) {
+      console.log(1)
+      router.push('./home')
+    }
+  })
+
   return (
     <main className="w-screen h-screen">
       <div className="w-screen h-screen mx-auto lg:py-16 lg:grid-cols-2 gap-8 lg:gap-16">
--- src/components/original/adminLoginForm.tsx
+++ src/components/original/adminLoginForm.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useRouter } from 'next/navigation'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from '../ui/button'
 import Link from 'next/link'
 import { Input } from '../ui/input'
